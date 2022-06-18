70ee6bb86f4d3541f192b9d7b383c6355d2c6145
H <98574414+hckkiu@users.noreply.github.com>
2022-06-18 12:55:02 +0800

    Add files via upload

--- wealth/wealth.py
+++ wealth/wealth.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
+
 
 df = pd.read_csv('dfa-networth-shares.csv')
-df['Year'] = df['Date'].apply(lambda x: int(x[:4]))
-df = df[['Net worth', 'Category', 'Year']].groupby(['Year', 'Category']).sum().unstack()
+df['Datex'] = pd.to_datetime(df['Date'].str.replace(':', '-'))
+df = df[['Net worth', 'Category', 'Datex']].groupby(['Datex', 'Category']).sum().unstack()
 df = df.apply(lambda x: x/x.sum() * 100, axis=1)
 df.loc[:, ('Net worth', 'Top10')] = df['Net worth']['Top1'] + df['Net worth']['Next9']
 df.loc[:, ('Net worth', 'Bottom90')] = df['Net worth']['Next40'] + df['Net worth']['Bottom50']
@@ -27,19 +30,23 @@ plt.yticks([25, 50, 75, 100], fontsize=14)
 plt.ylabel('Share of total wealth (%)', fontsize=14)
 
 ax.tick_params(axis='x', bottom=False)
-plt.xlim(1990, 2022)
-plt.xticks([1990, 2000, 2010, 2020], fontsize=14)
+plt.xlim(datetime.date(1990,1,1), datetime.date(2022,1,1))
+plt.xticks([datetime.date(i,1,1) for i in range(1990, 2021, 10)], fontsize=14)
+date_form = mdates.DateFormatter('%Y')
+ax.xaxis.set_major_formatter(date_form)
 
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
 for k, v in COLORS.items():
-    year = plt_df.loc[(plt_df['Category'] == k), 'Year']
+    year = plt_df.loc[(plt_df['Category'] == k), 'Datex']
     worth = plt_df.loc[(plt_df['Category'] == k), 'Net worth']
     ax.plot(year, worth, c=v)
     ax.plot(year.iloc[-1], worth.iloc[-1], 'o', c=v)
     ax.annotate(k + ' percent', 
-                xy=(year.iloc[-1]+1, worth.iloc[-1]),
+                xy=(year.iloc[-1], worth.iloc[-1]),
+                xytext=(10, -3), 
+                textcoords='offset points',
                 c=v, 
                 fontsize=12)
 
@@ -47,6 +54,7 @@ plt.title('The U.S. Household Wealth Distribution',
              fontsize=28, 
              loc='left', 
              x=-0.08, y=1.14)
+
 ax.text(s='The 10 percent wealthiest households possess almost \n70 percent of the total household wealth', 
         x=-.08, y=1.06, 
         fontsize=18, 
@@ -56,6 +64,7 @@ ax.text(s='H | Data: The Federal Reserve',
         x=0.5, y=-0.1,
         horizontalalignment='center',
         fontsize=14, 
-        transform=ax.transAxes, )
+        transform=ax.transAxes)
 
 fig.savefig('wealth', bbox_inches='tight')
+
